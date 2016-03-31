e07c6e96209d69da9a28fe42de53c5ee22b1fa4a
jiajian <601360343@qq.com>
2016-03-31 16:39:15 +0800

    publish

---
+++ src/Storage/VisitorInterface.php
@@ -0,0 +1,27 @@
+<?php namespace Weboap\Visitor\Storage;
+
+
+
+interface VisitorInterface {
+
+public function create( array $data);
+      
+public function get($ip);
+      
+public function update( $ip, array $data);     
+      
+public function delete( $ip );
+   
+public function all();
+    
+public function count( $ip );
+
+public function countArticleIp($ip, $id);
+   
+public function increment( $ip, $id );
+
+public function clicksSum();
+
+public function range( $start, $end);
+
+}
---
+++ src/config/visitor.php
@@ -0,0 +1,20 @@
+<?php  
+
+return [
+    
+    'table'   => 'visitor_registry',
+    
+    'ignored' => array(
+            '192.168.10.0/24',
+            '10.0.3.1',
+            '10.0.0.0/16',
+            '2001:14b8:100:934b::3:1',
+            '2001:14b8:100:934b::/64',
+            'localhost'
+    ),
+    
+    'maxmind_db_path' => storage_path().'/geo/GeoLite2-City.mmdb',
+    
+      
+  
+  ];
